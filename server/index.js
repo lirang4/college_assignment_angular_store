023e119e2a55ce99c56e0bfa013cc61cbd2d3f4f
Liran Glikman <liranglikm@gmail.com>
2019-02-02 20:36:32 +0200

    extract the scrapping to a function

--- server/index.js
+++ server/index.js
@@ -25,74 +25,78 @@ app.listen(port, () => {
     console.log("Running RestHub on port " + port);
 });
 
-for (let i = 1; i < 2; i++) {
-    let options = {
-        uri: `https://www.zap.co.il/models.aspx?sog=e-cellphone&pageinfo=${i}`,
-        transform: function (body) {
-            return cheerio.load(body);
-        }
-    };
+scrap_phones_data();
 
-    const phones = []
+function scrap_phones_data() {
+    for (let i = 1; i < 2; i++) {
+        let options = {
+            uri: `https://www.zap.co.il/models.aspx?sog=e-cellphone&pageinfo=${i}`,
+            transform: function (body) {
+                return cheerio.load(body);
+            }
+        };
 
-    rp(options)
-        .then(($) => {
-            $('.ProdGeneralInfo').each(function (i, elem) {
-                const phone = {};
+        const phones = []
 
-                $('.pair', elem).each(function (i, elem) {
-                    const result = $(this).text().trim().split(':');
-                    let key;
-                    switch (result[0]) {
-                        case 'יצרן':
-                            key = 'brand';
-                            break;
-                        case 'גודל מסך':
-                            key = 'screen_size';
-                            break;
-                        case 'נפח אחסון פנימי':
-                            key = 'memory_capacity';
-                            break;
-                        case 'שנת הכרזה':
-                            key = 'year';
-                            break;
-                        case 'ערכת שבבים':
-                            key = 'processor';
-                            break;
-                        case 'זיכרון RAM':
-                            key = 'ram';
-                            break;
-                        case 'רזולוציה':
-                            key = 'resulution';
-                            break;
-                        case 'מצלמה':
-                            key = 'camera';
-                            break;
-                        default:
-                            key = result[0];
-                            break;
-                    }
-                    phone[key] = result[1];
+        rp(options)
+            .then(($) => {
+                $('.ProdGeneralInfo').each(function (i, elem) {
+                    const phone = {};
+
+                    $('.pair', elem).each(function (i, elem) {
+                        const result = $(this).text().trim().split(':');
+                        let key;
+                        switch (result[0]) {
+                            case 'יצרן':
+                                key = 'brand';
+                                break;
+                            case 'גודל מסך':
+                                key = 'screen_size';
+                                break;
+                            case 'נפח אחסון פנימי':
+                                key = 'memory_capacity';
+                                break;
+                            case 'שנת הכרזה':
+                                key = 'year';
+                                break;
+                            case 'ערכת שבבים':
+                                key = 'processor';
+                                break;
+                            case 'זיכרון RAM':
+                                key = 'ram';
+                                break;
+                            case 'רזולוציה':
+                                key = 'resulution';
+                                break;
+                            case 'מצלמה':
+                                key = 'camera';
+                                break;
+                            default:
+                                key = result[0];
+                                break;
+                        }
+                        phone[key] = result[1];
+                    });
+
+                    const model = new Phone();
+                    model.brand = phone.brand;
+                    model.memory_capacity = phone.memory_capacity;
+                    model.screen_size = phone.screen_size;
+                    model.ram = phone.ram;
+
+                    phones.push(model);
                 });
 
-                const model = new Phone();
-                model.brand = phone.brand;
-                model.memory_capacity = phone.memory_capacity;
-                model.screen_size = phone.screen_size;
-                model.ram = phone.ram;
+                $('.pricesTxt').each(function (i, elem) {
+                    phones[i].price = $(this).text().trim();
+                });
 
-                phones.push(model);
+                for (const model of phones) {
+                    model.save();
+                }
+            })
+            .catch((err) => {
+                console.log(err);
             });
-
-            $('.pricesTxt').each(function (i, elem) {
-                phones[i].price = $(this).text().trim();
-            });
-
-            for (const model of phones) {
-                model.save();
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    }
 }
