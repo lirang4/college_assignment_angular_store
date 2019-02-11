1ab22d229fe2eb747b9ed34b0032458e5afacfd8
Liran Glikman <liranglikm@gmail.com>
2019-02-11 22:05:01 +0200

    Added graph for most phone for each brand

--- client/src/app/features/admin/admin.component.ts
+++ client/src/app/features/admin/admin.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as d3 from 'd3';
 
-export interface DataType { viewsNumber: number; }
+export interface DataType1 { viewsNumber: number; }
+export interface DataType2 { value: number; }
 
 @Component({
   selector: 'app-admin',
@@ -11,7 +12,8 @@ export interface DataType { viewsNumber: number; }
 })
 export class AdminComponent implements OnInit {
   colors: Array<string>;
-  data: any;
+  data1: Array<any>;
+  data2: Array<any>;
 
   constructor(private http: HttpClient) { }
 
@@ -20,14 +22,20 @@ export class AdminComponent implements OnInit {
 
     this.http.get('/api/mostViewed?top=5')
       .subscribe((res: any) => {
-        this.data = res.data;
-        this.createChart(res.data);
+        this.data1 = res.data;
+        this.createChart(this.data1);
+      });
+
+    this.http.get('/api/phoneBrandQuantity')
+      .subscribe((res: any) => {
+        this.data2 = res.data.results.slice(0, 5);
+        this.createChart2(this.data2);
       });
   }
 
   private createChart(data): void {
-    const width = 400;
-    const height = 400;
+    const width = 300;
+    const height = 300;
     const radius = Math.min(width, height) / 2 - 5;
 
     const svg = d3.select('#chart-area')
@@ -37,30 +45,13 @@ export class AdminComponent implements OnInit {
       .append('g')
       .attr('transform', `translate(${width / 2}, ${height / 2})`);
 
-    const pie = d3.pie<DataType>()
+    const pie = d3.pie<DataType1>()
       .value(d => d.viewsNumber)
       .sort(null);
 
     const arc: any = d3.arc()
       .innerRadius(0)
       .outerRadius(radius);
-
-    const labelArc = d3.arc()
-      .outerRadius(radius - 40)
-      .innerRadius(radius - 40);
-
-    const tooltip = d3.select('#chart') // select element in the DOM with id 'chart'
-      .append('div') // append a div element to the element we've selected
-      .attr('class', 'tooltip'); // add class 'tooltip' on the divs we just selected
-
-    tooltip.append('div') // add divs to the tooltip defined above
-      .attr('class', 'label'); // add class 'label' on the selection
-
-    tooltip.append('div') // add divs to the tooltip defined above
-      .attr('class', 'count'); // add class 'count' on the selection
-
-    tooltip.append('div') // add divs to the tooltip defined above
-      .attr('class', 'percent'); // add class 'percent' on the selection
 
     update(data, this.colors);
 
@@ -80,7 +71,52 @@ export class AdminComponent implements OnInit {
       path.transition().duration(200).attrTween('d', arcTween);
 
       // Enter new arcs
+      path.enter().append('path')
+        .attr('fill', (d, i) => colors[i])
+        .attr('d', arc)
+        .attr('stroke', 'white')
+        .attr('stroke-width', '6px');
+    }
+  }
 
+  private createChart2(data): void {
+    const width = 300;
+    const height = 300;
+    const radius = Math.min(width, height) / 2 - 5;
+
+    const svg = d3.select('#chart-area2')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', `translate(${width / 2}, ${height / 2})`);
+
+    const pie = d3.pie<DataType2>()
+      .value(d => d.value)
+      .sort(null);
+
+    const arc: any = d3.arc()
+      .innerRadius(0)
+      .outerRadius(radius);
+
+    update(data, this.colors);
+
+    function arcTween(a) {
+      const i = d3.interpolate(this._current, a);
+      this._current = i(1);
+      return (t: any) => arc(i(t));
+    }
+
+    function update(graphData: any, colors: Array<string>) {
+      // Join new data
+      const arcs = pie(data);
+      const path = svg.selectAll('path')
+        .data(pie(graphData));
+
+      // Update existing arcs
+      path.transition().duration(200).attrTween('d', arcTween);
+
+      // Enter new arcs
       path.enter().append('path')
         .attr('fill', (d, i) => colors[i])
         .attr('d', arc)
