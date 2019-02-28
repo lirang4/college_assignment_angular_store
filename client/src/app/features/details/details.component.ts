8271a858115d186983f35c6f0a8b94695dfd2020
Liran Glikman <liranglikm@gmail.com>
2019-02-28 19:05:50 +0200

    fix last stuff

--- client/src/app/features/details/details.component.ts
+++ client/src/app/features/details/details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AuthService } from 'src/app/core/services/auth.service';
 
@@ -13,16 +13,19 @@ export class DetailsComponent implements OnInit {
   stores: any;
 
   constructor(
+    private router: Router,
     private authService: AuthService,
     private route: ActivatedRoute,
     private http: HttpClient) { }
 
   ngOnInit() {
+    if (!this.route.snapshot.data.message) {
+      return;
+    }
     this.phone = this.route.snapshot.data.message.data;
 
     this.http.get(`/api/availableStores/${this.phone._id}`).subscribe((res: { data: any }) => {
       this.stores = res.data[0];
-      console.log(this.stores);
     });
   }
 
@@ -32,6 +35,10 @@ export class DetailsComponent implements OnInit {
 
   onUpdate(phone: any): void {
     const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
-    this.http.post(`/api/phone/${phone._id}`, JSON.stringify(phone), { headers: headers }).subscribe();
+    this.http.post(`/api/phone/${phone._id}`, JSON.stringify(phone), { headers: headers }).subscribe((res) => {
+      if (res === null) {
+        this.router.navigate(['/home']);
+      }
+    });
   }
 }
